8d73b401a2bbef4eb43e7fdc345071dd72ac4bc5
JaedonSpurlock01 <112233517+JaedonSpurlock01@users.noreply.github.com>
2024-12-06 23:32:16 -0800

    feat: Added year filter on team page (doesn't do anything yet)

--- src/features/team/components/member-filter.tsx
+++ src/features/team/components/member-filter.tsx
@@ -1,8 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Filter, Search } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Calendar, Filter, Search } from "lucide-react";
+import { useState } from "react";
 
 export default function MembersFilter() {
+  const [currentYear, setCurrentYear] = useState(2025);
+
   return (
     <div className="bg-white rounded-sm border border-border p-6">
       <div className="space-y-6">
@@ -24,11 +28,34 @@ export default function MembersFilter() {
               Filter by branch
             </div>
             <div className="flex flex-wrap gap-2">
-              {["Technical", "Interview"].map((type) => (
+              {["Technical", "Interview", "Marketing", "Other"].map((type) => (
                 <Button variant="outline">{type}</Button>
               ))}
             </div>
           </div>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <div className="flex items-center gap-2 text-sm font-medium text-gray-700">
+            <Calendar className="w-4 h-4" />
+            Filter by year
+          </div>
+          <ul className="hidden sm:flex flex-row gap-2 h-full">
+            {[2025, 2024].map((year, index) => (
+              <li key={index}>
+                <Button
+                  variant="nav"
+                  className={cn(
+                    "text-foreground/70 h-full border-b-4 text-sm rounded-none px-4 m-0 hover:text-foreground transition",
+                    currentYear === year ? "border-blue" : "border-background"
+                  )}
+                  onClick={() => setCurrentYear(year)}
+                >
+                  {year}
+                </Button>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
